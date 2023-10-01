6bc12d2e5ecaa35008360d8404a6b51ab758ba1d
bordo <wedobest755@gmail.com>
2023-10-01 16:17:23 +0300

    next

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,8 +3,69 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Order;  // Исправлено здесь
 
 class OrderController extends Controller
 {
-    //
+    // Отображение списка всех заказов
+    public function index()
+    {
+        $orders = Order::all();
+        return view('orders.index', ['orders' => $orders]);
+    }
+
+    // Создание нового заказа
+    public function create()
+    {
+        return view('orders.create');
+    }
+
+    // Сохранение нового заказа
+    public function store(Request $request)
+    {
+        $order = new Order();
+        $order->clientId = $request->clientId;
+        $order->serviceId = $request->serviceId;
+        $order->status = $request->status;
+        $order->orderDate = $request->orderDate;
+        $order->save();
+
+        return redirect('/orders');
+    }
+
+    // Отображение конкретного заказа
+    public function show($id)
+    {
+        $order = Order::find($id);
+        return view('orders.show', ['order' => $order]);
+    }
+
+    // Редактирование заказа
+    public function edit($id)
+    {
+        $order = Order::find($id);
+        return view('orders.edit', ['order' => $order]);
+    }
+
+    // Обновление заказа
+    public function update(Request $request, $id)
+    {
+        $order = Order::find($id);
+        $order->clientId = $request->clientId;
+        $order->serviceId = $request->serviceId;
+        $order->status = $request->status;
+        $order->orderDate = $request->orderDate;
+        $order->save();
+
+        return redirect('/orders');
+    }
+
+    // Удаление заказа
+    public function destroy($id)
+    {
+        $order = Order::find($id);
+        $order->delete();
+
+        return redirect('/orders');
+    }
 }
--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -4,7 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Portfolio;
+
+use Illuminate\Routing\Controller;
+
 class PortfolioController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $request->validate([
+            'projectName' => 'required|string|max:255',
+            'description' => 'required|string',
+            'imageURL' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'completionDate' => 'required|date'
+        ]);
+
+        $portfolio = new Portfolio();
+        $portfolio->projectName = $request->projectName;
+        $portfolio->description = $request->description;
+        $portfolio->imageURL = $request->imageURL;
+        $portfolio->completionDate = $request->completionDate;
+        $portfolio->save();
+
+        return redirect()->route('portfolio.index')->with('success', 'Portfolio item created successfully.');
+    }
 }
